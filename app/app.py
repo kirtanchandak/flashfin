ecf2deeed0281ea57c970b97ae7a82fd4629169c
scikido <sarvadnyachavhan07@outlook.com>
2023-04-12 19:38:46 +0530

    added sending mail functionality

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests,json
 from dotenv import load_dotenv
 import os
+import ssl
+import smtplib
 
 load_dotenv()
 
@@ -19,11 +21,20 @@ if response.status_code == 200:
 else:
     print(f'Error fetching data from {url}, status code: {response.status_code}')
 
-
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('index.html', emails=emails)
+context = ssl.create_default_context()
+@app.route("/mail")
+def mail():
+    if request.method == 'POST':
+       
+        with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+            smtp.starttls(context=context)
+            smtp.login(email_sender, email_password)  # type: ignore
+            smtp.sendmail(email_sender, email_reciever, em.as_string())  # type: ignore
+            print('Sent')
+        return render_template('mail.html',email=email)
    
