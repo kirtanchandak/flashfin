a79d2a024d20b2afe2c5c0ee35fd3dafcf918860
kirtanchandak <themarketingkirtan@gmail.com>
2023-04-16 00:50:20 +0530

    mongodb setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,45 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
 
 let emails = [];
 console.log(emails);
+
+// Connecting to MongoDB and defining collection
+const url = process.env.MONGO_URL;
+const dbName = "flashfin";
+const client = new MongoClient(url);
+client.connect(function (err) {
+  console.log("Connected successfully to server");
+});
+
+// name of the collection
+const collectionName = "userEmails";
+
+// fetching exixting data
+const getInputsFromDb = async () => {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.findOne({ name: "inputs" });
+  return result ? result.inputs : [];
+};
+
+getInputsFromDb()
+  .then((result) => {
+    emails = result;
+    console.log(emails);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.post("/submit", async (req, res) => {
   let userInput = req.body.userInput;
