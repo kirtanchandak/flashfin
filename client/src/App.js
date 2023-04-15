4b24890c35fe20e72a339deabb0b3be0c1422563
kirtanchandak <themarketingkirtan@gmail.com>
2023-04-16 01:36:29 +0530

    homepage v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 
 function App() {
@@ -27,13 +28,41 @@ function App() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter your input:
-        <input type="" value={inputValue} onChange={handleChange} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <React.Fragment>
+      <section class="body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-col text-center w-full mb-12">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4">
+              Master Cleanse Reliac Heirloom
+            </h1>
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+              gentrify, subway tile poke farm-to-table. Franzen you probably
+              haven't heard of them man bun deep.
+            </p>
+          </div>
+          <div class="flex justify-center items-center">
+            <label for="email" class="leading-7 text-sm">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              class="w-72 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-transparent focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              value={inputValue}
+              onChange={handleChange}
+            />
+            <button
+              class="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg"
+              onClick={handleSubmit}
+            >
+              Button
+            </button>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
   );
 }
 
