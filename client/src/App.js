2ce9163b67cd6e5ac024e2191457dae66c5ad583
kirtanchandak <themarketingkirtan@gmail.com>
2023-04-16 02:05:59 +0530

    home ui v2

--- client/src/App.js
+++ client/src/App.js
@@ -32,33 +32,43 @@ function App() {
       <section class="body-font">
         <div class="container px-5 py-24 mx-auto">
           <div class="flex flex-col text-center w-full mb-12">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4">
-              Master Cleanse Reliac Heirloom
+            <h1 class="sm:text-3xl text-2xl title-font mb-4 font-bold">
+              FlashFin 🤑
             </h1>
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify, subway tile poke farm-to-table. Franzen you probably
-              haven't heard of them man bun deep.
+              We, give you fintech news, at your fingertips, on your own email.
+              <br />
+              <span className="">Sign Up, now!!</span>
             </p>
           </div>
-          <div class="flex justify-center items-center">
-            <label for="email" class="leading-7 text-sm">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              class="w-72 bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-transparent focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              value={inputValue}
-              onChange={handleChange}
-            />
-            <button
-              class="text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg"
-              onClick={handleSubmit}
-            >
-              Button
-            </button>
+          <div class="flex flex-col justify-center items-center">
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+              <div class="mb-4">
+                <label
+                  class="block text-gray-700 font-bold mb-2"
+                  for="username"
+                >
+                  Email
+                </label>
+                <input
+                  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="username"
+                  type="email"
+                  placeholder="Enter your email"
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div class="flex items-center justify-center">
+                <button
+                  class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="button"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </section>
